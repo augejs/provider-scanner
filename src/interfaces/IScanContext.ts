f00088801575139944f2ecc290d1568b37963d3b
alex <alex@alex.com>
2020-09-17 20:24:02 +0800

    feat: refactor the scan struct

--- src/interfaces/IScanContext.ts
+++ src/interfaces/IScanContext.ts
@@ -7,22 +7,9 @@ import { IScanNode } from './IScanNode';
  */
 export interface IScanContext {
   /**
-   * the inputs of scan context
-   */
-  inputs: Map<any, any>
-
-  /**
-   * the outputs of scan context
-   */
-  outputs: Map<any, any>
-
-  /**
-   * the time cost of scan
-   */
-  timeCost: number
-
-  /**
    * the root scan node
    */
-  rootScanNode: IScanNode | null
+  rootScanNode?: IScanNode
+
+  [prop: string]: any
 }
